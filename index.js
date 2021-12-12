a7d8852b9cb934e67a2fec5adbe60c7b24084a34
Aaron Elias <aaronelias007@gmail.com>
2021-12-11 22:49:47 -0500

    commit tree 0358836cd9548c4bc850551f9bbbe5728e755f6a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return(`This Saturday, I want to ${activity}!`);
+  }
+  saturdayFun();
+  saturdayFun('play ball');
+
+  function mondayWork(work = 'go to the office') {
+    return(`This Monday, I will ${work}.`);
+  }
+  function wrapAdjective(star = '*'){
+    return function (name = 'special'){
+        return (`You are ${star}${name}${star}!`);
+
+      }
+  }
